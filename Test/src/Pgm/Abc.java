74e7a7c6dbfa1c978b02b2510c11d7d9087b3f4b
HartickSM <harthicksm@gmail.com>
2023-09-13 18:47:12 +0530

    abbc

---
+++ Test/src/Pgm/Abc.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package Pgm;
+
+/**
+ * 
+ */
+public class Abc {
+
+	/**
+	 * 
+	 */
+	public Abc() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("happyjava");
+
+	}
+
+}
